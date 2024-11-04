0669b1122eeab5e6624f409831737d08e5335a2d
linciss <linardssils1@gmail.com>
2024-11-04 14:35:32 +0200

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Alert } from "@material-tailwind/react";
-import { Spinner } from "@material-tailwind/react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <main>
       <h1 className="text-red-500">Ur mother is my hoether</h1>
-      <Alert>
-        <Spinner />
-      </Alert>
-    </>
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path="/" component={<Home />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
