5a15dbed45b8ec57b341e9cb30781d939c4f306f
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-11-05 15:46:46 +0200

    Adding new accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      accent1: "#A53F4F",
+    },
     extend: {},
   },
   plugins: [],
