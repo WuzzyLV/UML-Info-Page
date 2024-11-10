d4f01ed88f79c6da30e29dc5cd097997d559e43a
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-11-10 20:08:53 +0200

    make sidebar rerender active cunts on location change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 // NavLink component
 const NavLink = ({ ...props }) => {
   const { children, href = "", className = "", active = "" } = props;
 
-  const [pathname, setPathname] = useState("/");
-
-  const isActive = pathname == href;
-  const activeClass = isActive ? active : "";
+  const [isActive, setIsActive] = useState("");
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    setPathname(window.location.pathname);
-  }, [props]);
+    const handleRouteChange = () => {
+      if (window.location.pathname === href) {
+        setIsActive(active);
+      } else {
+        setIsActive("");
+      }
+    };
+
+    handleRouteChange(); // Check on initial render
+    window.addEventListener("popstate", handleRouteChange);
+
+    return () => {
+      window.removeEventListener("popstate", handleRouteChange);
+    };
+  }, [pathname]);
 
   return (
-    <a href={href} {...props} className={`${activeClass} ${className}`}>
+    <a href={href} {...props} className={`${isActive} ${className}`}>
       {children}
     </a>
   );
@@ -43,36 +55,12 @@ const SectionsList = ({ items }) => (
   </div>
 );
 
-// Search Box component
-const SearchBox = ({ ...props }) => (
-  <div className="relative w-full">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      className="w-5 h-5 text-gray-400 absolute left-3 inset-y-0 my-auto"
-    >
-      <path
-        fillRule="evenodd"
-        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-        clipRule="evenodd"
-      />
-    </svg>
-
-    <input
-      {...props}
-      type="email"
-      className="w-full pl-12 pr-3 py-2 bg-white text-sm text-gray-500 bg-transparent outline-none border ring-blue-600 focus:ring-2 shadow-sm rounded-lg duration-200"
-    />
-  </div>
-);
-
 const Sidebar = () => {
   const lessons = {
     classDiagram: [
-      { name: "Introduction to Rust", href: "javascript:void(0)" },
-      { name: "Installing and Setting up Rust", href: "javascript:void(0)" },
-      { name: "Basic Syntax and Data Types", href: "javascript:void(0)" },
+      { name: "Kapēc un kā", href: "/class-diagram" },
+      { name: "Sintakse", href: "/class-diagram/syntax" },
+      { name: "Relācija", href: "/class-diagram/relation" },
     ],
     activityDiagram: [
       { name: "Introduction to Cargo", href: "javascript:void(0)" },
