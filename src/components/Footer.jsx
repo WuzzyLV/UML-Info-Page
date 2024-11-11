aae2f37babccf3999418ea31f3d249598933bc40
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-11-11 10:10:34 +0200

    Make footer more clickable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,14 +18,14 @@ export const Footer = () => {
     <footer className="w-full mb-12">
       <div className="flex justify-center">
         <nav
-          className="-mb-px flex border border-gray-400 rounded-lg shadow-md px-4"
+          className="-mb-px flex border border-gray-400 rounded-lg shadow-md"
           aria-label="Tabs"
         >
           {/* Previous Button */}
           <button
             onClick={() => prevPage && navigate(prevPage.path)}
             disabled={!prevPage}
-            className={`inline-flex shrink-0 items-center gap-2 border-transparent px-1 pr-4 border-r border-r-accent1 py-1 my-1 text-md font-medium ${
+            className={`inline-flex shrink-0 items-center gap-2 border-transparent px-1 px-4 border-r border-r-accent1 py-1 my-1 text-md font-medium ${
               prevPage
                 ? "text-gray-500 hover:border-gray-300 hover:text-gray-700"
                 : "text-gray-300 cursor-not-allowed"
@@ -38,7 +38,7 @@ export const Footer = () => {
           <button
             onClick={() => nextPage && navigate(nextPage.path)}
             disabled={!nextPage}
-            className={`inline-flex shrink-0 items-center gap-2 border-transparent px-1 pl-4 border-l border-l-accent1 py-1 my-1 text-md font-medium ${
+            className={`inline-flex shrink-0 items-center gap-2 border-transparent px-1 px-4 border-l border-l-accent1 py-1 my-1 text-md font-medium ${
               nextPage
                 ? "text-gray-500 hover:border-gray-300 hover:text-gray-700"
                 : "text-gray-300 cursor-not-allowed"
