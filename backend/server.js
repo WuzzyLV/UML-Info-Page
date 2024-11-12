e4d6cad2f555e4128be37b454c8023a77d5e881f
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-11-13 00:35:40 +0200

    +

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,10 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
-app.get("/hello", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
-});
-
 //404
 app.use((req, res, next) => {
   console.log(req);
-  res.status(404).json({ message: req.path + "Not found" });
+  res.status(404).json({ message: req.path + " Not found" });
 });
 
 app.listen(port, () => {
