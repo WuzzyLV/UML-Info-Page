823e6bb2964dca0040f5e70f81b636bf4d64f251
lincis <linardssils1@gmail.com>
2024-11-12 22:47:04 +0200

    feat: Add ClassExample page to class diagram section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { ClassRelation } from "./pages/ClassRelation";
 import { ActivityHome } from "./pages/ActivityHome";
 import { ActivitySwimlane } from "./pages/ActivitySwimlane";
 import { ActivityNotations } from "./pages/ActivityNotations";
+import { ClassExample } from "./pages/ClassExample";
 // pages.js
 export const pages = [
   { id: 1, title: "Ievads", path: "/", content: <Home /> },
@@ -28,6 +29,12 @@ export const pages = [
     title: "Relācija",
     path: "/class-diagram/relation",
     content: <ClassRelation />,
+  },
+  {
+    id: 9,
+    title: "Piemēri",
+    path: "/class-diagram/examples",
+    content: <ClassExample />,
   },
   {
     id: 5,
