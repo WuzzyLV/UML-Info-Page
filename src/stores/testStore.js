080f6d0d5ff22d965a638daa07e56e61516b0263
lincis <linardssils1@gmail.com>
2024-11-13 00:03:58 +0200

    feat: Add questionNumber and answers to testStore

--- src/stores/testStore.js
+++ src/stores/testStore.js
@@ -2,5 +2,22 @@ import { create } from "zustand";
 
 export const useTestStore = create((set, get) => ({
   open: false,
+  questionNumber: 0,
+  answers: [],
   setOpen: (open) => set({ open }),
+  setQuestionNumber: (questionNumber) => set({ questionNumber }),
+  setAnswers: (newAnswer) =>
+    set((state) => {
+      const existingAnswerIndex = state.answers.findIndex(
+        (answer) => answer.questionId === newAnswer.questionId
+      );
+
+      if (existingAnswerIndex !== -1) {
+        const updatedAnswers = [...state.answers];
+        updatedAnswers[existingAnswerIndex] = newAnswer;
+        return { answers: updatedAnswers };
+      } else {
+        return { answers: [...state.answers, newAnswer] };
+      }
+    }),
 }));
