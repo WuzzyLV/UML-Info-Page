04ccfab1d6a70363f91f55802a93a63e2e2c9179
lincis <linardssils1@gmail.com>
2024-11-12 18:42:29 +0200

    refactor: Remove unused imports and update swimlane description in ActivitySwimlane component

--- src/pages/ClassRelation.jsx
+++ src/pages/ClassRelation.jsx
@@ -1,7 +1,3 @@
-import { Typography } from "@material-tailwind/react";
-import notationExample from "../assets/classNotation.svg";
-import visibilityExample from "../assets/classVisibility.svg";
-import ImageWithComments from "../components/ImageWithComments";
 import associationExample from "../assets/classRelationships/assoc.png";
 import inheritanceExample from "../assets/classRelationships/inher.svg";
 import aggregationExample from "../assets/classRelationships/aggreg.svg";
@@ -78,31 +74,6 @@ const HeroSection = () => {
               </li>
             ))}
           </ul>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-const VisibilitySection = () => {
-  return (
-    <section className="h-screen flex items-center -mb-8">
-      <div className="max-w-screen-xl mx-auto px-4  gap-x-12 justify-between md:flex md:px-8">
-        <div className="flex-none mt-4 md:mt-0">
-          <ImageWithComments
-            src={visibilityExample}
-            alt="Annotated Image"
-            comments={[]}
-          />
-        </div>
-        <div className="max-w-xl flex flex-col justify-center">
-          <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-            Redzamība
-          </h3>
-          <p className="mt-3 text-gray-600">
-            Simboli +, - un # pirms atribūta un operācijas nosaukuma klasē
-            apzīmē redzamību.
-          </p>
         </div>
       </div>
     </section>
